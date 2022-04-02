da29fc82f5ebf295fcbb3404ca51ce45f162eed2
Michael Sergeev <michael.sergeew@gmail.com>
2022-04-02 15:59:13 +0700

    Add first screen of application

---
+++ ExchangeRate/Model/Settings.swift
@@ -0,0 +1,36 @@
+//
+//  Settings.swift
+//  ExchangeRate
+//
+//  Created by Mikhail Sergeev on 02.04.2022.
+//
+
+import Foundation
+
+class Settings {
+    enum Table {
+        enum SettingDisplayTitles: String, CaseIterable {
+            case favourites = "Избранные валюты"
+            case other = "Другие валюты"
+        }
+        
+        enum RatingDisplayTitles: String, CaseIterable {
+            case currency = "Выбранная валюта"
+        }
+    }
+    
+    enum Favourites: Int {
+        case min = 1
+        case max = 5
+    }
+    
+    enum DefaultCurrency: String, CaseIterable {
+        case usd = "USD"
+    }
+    
+    enum UserDefaultKeys: String, CaseIterable {
+        case favorites
+    }
+}
+
+
